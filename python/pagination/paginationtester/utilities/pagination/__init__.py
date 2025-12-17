d8b6531d7044ade95cfe48f798ce7d3bde6669aa
DESKTOP-10GD4VQ\Sufyan <muhammad.sufyan@apimatic.io>
2025-12-17 21:36:08 +0500

    feature sdk

--- python/pagination/paginationtester/utilities/pagination/__init__.py
+++ python/pagination/paginationtester/utilities/pagination/__init__.py
@@ -1,4 +1,6 @@
+# ruff: noqa: D104 | Missing docstring in public package
+# ruff: noqa: RUF022 | `__all__` is not sorted
 __all__ = [
-    'paged_iterable',
-    'paged_response',
+    "paged_iterable",
+    "paged_response",
 ]
