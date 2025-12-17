a4e2dbce3a0789db3771fb7947bc12d784d40f85
DESKTOP-10GD4VQ\Sufyan <muhammad.sufyan@apimatic.io>
2025-12-17 18:34:39 +0500

    feature-sdk

--- additionalProperties/additionalpropertiestester/__init__.py
+++ additionalProperties/additionalpropertiestester/__init__.py
@@ -1,10 +1,12 @@
+# ruff: noqa: D104 | Missing docstring in public package
+# ruff: noqa: RUF022 | `__all__` is not sorted
 __all__ = [
-    'additionalpropertiestester_client',
-    'api_helper',
-    'configuration',
-    'controllers',
-    'exceptions',
-    'http',
-    'models',
-    'utilities',
+    "additionalpropertiestester_client",
+    "api_helper",
+    "configuration",
+    "controllers",
+    "exceptions",
+    "http",
+    "models",
+    "utilities",
 ]
--- additionalProperties/additionalpropertiestester/controllers/__init__.py
+++ additionalProperties/additionalpropertiestester/controllers/__init__.py
@@ -1,5 +1,7 @@
+# ruff: noqa: D104 | Missing docstring in public package
+# ruff: noqa: RUF022 | `__all__` is not sorted
 __all__ = [
-    'base_controller',
-    'body_controller',
-    'form_controller',
+    "base_controller",
+    "body_controller",
+    "form_controller",
 ]
